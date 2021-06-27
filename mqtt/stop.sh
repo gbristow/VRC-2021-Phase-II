3bb32a8a420aaa1593eb081bf94da5cbc65fcc3c
Nathan Vaughn <nvaughn51@gmail.com>
2021-06-27 13:55:15 -0500

    Add some mosquitto scripts

---
+++ mqtt/stop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# see if sudo is installed
+# mainly for testing with Docker, that doesn't have sudo
+s=""
+if [ -n "$(which sudo)" ]; then
+    s="sudo"
+fi
+
+$s service mosquitto stop
