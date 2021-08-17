10e2758a3f42824b638ec89a854575305b619314
Casey Hanner <bhanner95@gmail.com>
2021-08-16 22:19:22 -0500

    trying simple

---
+++ vmc/flight_control_module/simple_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import asyncio
+from mavsdk import System
+async def run():
+    drone = System()
+    await drone.connect(system_address="udp://:14541")
+    print("Waiting for drone...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"Drone discovered with UUID: {state.uuid}")
+            break
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
