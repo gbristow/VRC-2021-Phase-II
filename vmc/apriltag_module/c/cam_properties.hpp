e015de0059f97632ad21df5f1f66ce184a2d49da
Casey Hanner <bhanner95@gmail.com>
2021-09-06 17:21:24 -0500

    external

--- vmc/apriltag_module/c/cam_properties.hpp
+++ vmc/apriltag_module/c/cam_properties.hpp
@@ -1,12 +1,12 @@
 #ifndef CAM_PROPERTIES_H
 #define CAM_PROPERTIES_H
 
-extern int width = 1280;
-extern int height = 720;
+extern int width;
+extern int height;
 
-extern float fx = 784.0756786399139;
-extern float fy = 784.9009527658286;
-extern float ppx = 677.124825443364;
-extern float ppy = 385.33983488708003;
+extern float fx;
+extern float fy;
+extern float ppx;
+extern float ppy;
 
 #endif
