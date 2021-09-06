995fc9a7e4356669aa1ad3e445e48c40d2e47f99
Casey Hanner <bhanner95@gmail.com>
2021-09-06 16:39:46 -0500

    vpi imports

--- vmc/apriltag_module/c/undistort.hpp
+++ vmc/apriltag_module/c/undistort.hpp
@@ -1,35 +1,39 @@
-#include <opencv2/core/version.hpp>
-
-#if CV_MAJOR_VERSION >= 3
-#    include <opencv2/imgcodecs.hpp>
-#else
-#    include <opencv2/highgui/highgui.hpp>
-#endif
-
-#include <opencv2/opencv.hpp>
-
-#include "vpi/OpenCVInterop.hpp"
-#include "vpi/Image.h"
-#include "vpi/LensDistortionModels.h"
-#include "vpi/Status.h"
-#include "vpi/Stream.h"
-#include "vpi/algo/ConvertImageFormat.h"
-#include "vpi/algo/Remap.h"
-
-
-#define CHECK_STATUS(STMT)                                    \
-    do                                                        \
-    {                                                         \
-        VPIStatus status = (STMT);                            \
-        if (status != VPI_SUCCESS)                            \
-        {                                                     \
-            char buffer[VPI_MAX_STATUS_MESSAGE_LENGTH];       \
-            vpiGetLastStatusMessage(buffer, sizeof(buffer));  \
-            std::ostringstream ss;                            \
-            ss << vpiStatusGetName(status) << ": " << buffer; \
-            throw std::runtime_error(ss.str());               \
-        }                                                     \
-    } while (0);
+ #include <opencv2/core/version.hpp>
+  
+ #if CV_MAJOR_VERSION >= 3
+ #    include <opencv2/imgcodecs.hpp>
+ #else
+ #    include <opencv2/highgui/highgui.hpp>
+ #endif
+ 
+ #include <opencv2/imgproc/imgproc.hpp>
+ #include <vpi/OpenCVInterop.hpp>
+  
+ #include <string.h> // for basename(3) that doesn't modify its argument
+ #include <unistd.h> // for getopt
+ #include <vpi/Image.h>
+ #include <vpi/LensDistortionModels.h>
+ #include <vpi/Status.h>
+ #include <vpi/Stream.h>
+ #include <vpi/algo/ConvertImageFormat.h>
+ #include <vpi/algo/Remap.h>
+  
+ #include <iostream>
+ #include <sstream>
+  
+ #define CHECK_STATUS(STMT)                                    \
+     do                                                        \
+     {                                                         \
+         VPIStatus status = (STMT);                            \
+         if (status != VPI_SUCCESS)                            \
+         {                                                     \
+             char buffer[VPI_MAX_STATUS_MESSAGE_LENGTH];       \
+             vpiGetLastStatusMessage(buffer, sizeof(buffer));  \
+             std::ostringstream ss;                            \
+             ss << vpiStatusGetName(status) << ": " << buffer; \
+             throw std::runtime_error(ss.str());               \
+         }                                                     \
+     } while (0);
 
 //###################################################################### V P I ####################################################################
 
