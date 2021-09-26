1cc280a256a8f3179a10679e2df8053185521d3e
Nathan Vaughn <nvaughn51@gmail.com>
2021-09-26 16:26:15 -0500

    Start clean up

--- vmc/apriltag_module/start_module.sh
+++ vmc/apriltag_module/start_module.sh
@@ -1,4 +1,6 @@
-#run the linker because it couldnt do it at build time for some reason
+#!/bin/bash
+
+# run the linker because it couldnt do it at build time for some reason
 ldconfig /opt/nvidia/vpi1/lib64
-#run the c program
+# run the c program
 python3 python/apriltag_processor.py
